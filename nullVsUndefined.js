72cb4342965015a5664d93cb4926294cc3ee9d89
spaceled <spaceled@gmail.com>
2020-08-07 10:19:18 +0600

    Done

---
+++ nullVsUndefined.js
@@ -0,0 +1,17 @@
+function add(num1, num2) {
+    console.log(num1+num2);
+    return num2
+}
+
+function add(num1, num2) {
+    console.log(num1, num2);
+}
+
+const premik = {name: 'smart dude', phone: 4561222};
+console.log(premik.gf);
+
+let fun = undefined;
+console.log(fun);
+
+let ages = [2, 6, 9];
+console.log(ages[11]);
