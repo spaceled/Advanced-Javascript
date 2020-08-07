d8ab03e1730b238f3829c38b00da762815ef91b2
spaceled <spaceled@gmail.com>
2020-08-07 11:25:11 +0600

    Map, Filter, Find

---
+++ mapFilter.js
@@ -0,0 +1,36 @@
+const numbers = [3, 4, 5, 6, 8];
+const output = [];
+
+for (let i = 0; i < numbers.length; i++) {
+    const element = numbers[i];
+    const result = element * element;
+    output.push(result);
+}
+
+// -------------One way------------
+function square(element) {
+    return element * element;
+}
+
+//  Map: can take 3 parameters
+
+numbers.map(square)
+
+const square = element => element * element;
+const square = x => x * x;
+
+// ------------ Easy Way-----------
+const result = numbers.map(function(element){
+    return element * element;
+})
+const result = numbers.map(x => x * x);
+// console.log(output);
+console.log(result);
+
+//  Filter: To select something
+const result = numbers.filter(x => x > 5);
+console.log(result);
+
+// Find
+const isThere = numbers.find(x => 5);
+console.log(isThere);
