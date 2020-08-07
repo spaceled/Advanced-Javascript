71ab4f11b376955b76580d6b33703d5c2512e746
spaceled <spaceled@gmail.com>
2020-08-07 08:53:02 +0600

    True vs False

---
+++ trueFalse.js
@@ -0,0 +1,21 @@
+// ------------- Falsy Value
+// 0
+// ""
+// Undefined
+// null
+// NaN
+// false
+// ---------------------
+
+// ---------------Truthy Value
+// '0' ' ' [] {}
+
+const age = "0";
+let name;
+console.log(name);
+if (age) {
+    console.log("Condition is true");
+}
+else {
+    console.log('Condition is False');
+}
